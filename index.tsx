7f4bf912b96cc450e29358272f73700f0cdfb000
shelfscoutnet-lgtm <info@shelfscoutja.com>
2025-12-20 13:54:38 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
 
 // SAFETY NET: This catches the crash and shows the error on screen
 try {
